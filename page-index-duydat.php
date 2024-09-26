241209537828e886dc77a85c60781bc3f527a6f6
datdnit <datdnk3@gmail.com>
2024-09-26 12:21:49 +0700

    special-offer and new even

--- page-index-duydat.php
+++ page-index-duydat.php
@@ -14,16 +14,48 @@
     <!-- Include Header -->
     <?php
     include "./component-header.php"
-    ?>
+        ?>
     <!-- End Include Header -->
     <main>
-
-        <section class="component-top-tour">
-            <?php for ($i = 0; $i < 4; $i++): ?>
-                <div class="component-card-tour">1</div>
-            <?php endfor; ?>
+        <section id="special-offer" class="component-top-tour">
+            <div class="container">
+                <h2>Ưu đãi đặc biệt</h2>
+                <div class="owl-carousel owl-theme owl-carousel-one">
+                    <?php for ($i = 0; $i < 6; $i++) { ?>
+                        <div class="item">
+                            <a href="#"><img src="/assets/imgs/special_offer.jpg" alt=""></a>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
         </section>
 
+        <section id="new-event" class="component-top-tour">
+            <div class="container">
+                <h2>Tin tức và sự kiện</h2>
+                <div class="owl-carousel owl-theme owl-carousel-two">
+                    <?php for ($i = 0; $i < 6; $i++) { ?>
+                        <div class="item">
+                            <div class="item-img">
+                                <a href="#"><img src="/assets/imgs/special_offer.jpg" alt=""></a>
+                            </div>
+                            <div class="item-content">
+                                <p class="item__date">24-09-2024</p>
+                                <h3 class="item__title">Cập nhật thời tiết hàn quốc và kinh nghiệm du lịch theo mùa không
+                                    thể bỏ lỡ</h3>
+                                <p class="item__des">Hàn Quốc luôn là điểm đến thu hút khách du lịch từ khắp nơi đến tham
+                                    quan bởi thời tiết bốn mùa đều có sự thay đổi đa dạng, tạo nên nhiều cảnh quan xinh đẹp.
+                                    Để đảm bảo chuyến đi của du khách trở nên trọn vẹn hơn, việc cập nhật thời tiết Hàn Quốc
+                                    là điều vô cùng cần thiết. Dù là mùa xuân hoa anh ...</p>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+                <div class="new-event_button">
+                    <button type="button" class="btn btn-style">Xem Thêm</button>
+                </div>
+            </div>
+        </section>
     </main>
     <!-- Include Footer -->
     <?php include "./component-footer.php" ?>
